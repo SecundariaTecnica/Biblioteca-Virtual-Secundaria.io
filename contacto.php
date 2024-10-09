b2b26fac3ef7f0b0a487f295272e1dbaa9bedf02
SecundariaTecnica <proytecnica@gmail.com>
2024-10-09 13:44:07 -0600

    initial commit

---
+++ contacto.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "SecundariaTecnica";
+$password = "Proytecnica6@";
+$dbname = "biblioteca";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("INSERT INTO contactos (nombre, email, asunto, mensaje) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $nombre, $email, $asunto, $mensaje);
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+if ($stmt->execute()) {
+    echo "Mensaje enviado exitosamente.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
